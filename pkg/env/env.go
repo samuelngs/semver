b0e8d145e64ca37ac9148bfb6fb9a6b52d2f1e5a
Samuel <sam@infinitely.io>
2016-06-08 19:44:26 -0600

    environment reader pkg

---
+++ pkg/env/env.go
@@ -0,0 +1,49 @@
+package env
+
+import (
+	"os"
+	"strconv"
+)
+
+// Raw to read environment key
+func Raw(name string, defs ...string) string {
+	var def string
+	for _, d := range defs {
+		def = d
+		break
+	}
+	if str := os.Getenv(name); str != "" {
+		return str
+	}
+	return def
+}
+
+// I64 to read environment key and return value in int64 format
+func I64(name string, defs ...int64) int64 {
+	var def int64
+	for _, d := range defs {
+		def = d
+		break
+	}
+	v := Raw(name)
+	i, err := strconv.ParseInt(v, 10, 64)
+	if err != nil {
+		return def
+	}
+	return i
+}
+
+// Int to read environment key and return value in int format
+func Int(name string, defs ...int) int {
+	var def int
+	for _, d := range defs {
+		def = d
+		break
+	}
+	v := Raw(name)
+	i, err := strconv.Atoi(v)
+	if err != nil {
+		return def
+	}
+	return i
+}
