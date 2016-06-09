7aebdf64dc25d55978f8305e891d7cbf83a6aa5a
Samuel <sam@infinitely.io>
2016-06-08 19:45:36 -0600

    redis backend interface

---
+++ backend/redis.go
@@ -0,0 +1,133 @@
+package backend
+
+import (
+	"fmt"
+	"strings"
+
+	"gopkg.in/redis.v3"
+
+	"github.com/samuelngs/semver/pkg/env"
+)
+
+// Redis backend for semver
+type Redis struct {
+	*Core
+	c *redis.Client
+}
+
+// Init method
+func (r *Redis) Init() error {
+	opts := &redis.Options{
+		Addr:       env.Raw("SEMVER_BACKEND_ADDR", "localhost:6379"),
+		DB:         env.I64("SEMVER_BACKEND_DB", 0),
+		MaxRetries: env.Int("SEMVER_BACKEND_RETRIES", 5),
+	}
+	r.c = redis.NewClient(opts)
+	return nil
+}
+
+// Name method
+func (r *Redis) Name() string {
+	return "redis"
+}
+
+// Path method
+func (r *Redis) Path(key *Key) string {
+	dir := fmt.Sprintf("semver:db:%s", key.id)
+	for _, str := range key.dirs {
+		dir += ":" + str
+	}
+	return dir
+}
+
+// Exists method
+func (r *Redis) Exists(key *Key) (bool, error) {
+	_, err := r.c.Get(r.Path(key)).Result()
+	if err == redis.Nil {
+		return false, nil
+	} else if err != nil {
+		return false, err
+	}
+	return true, nil
+}
+
+// Set method
+func (r *Redis) Set(val string, keys ...*Key) error {
+	items := make([]string, len(keys)*2)
+	for i, key := range keys {
+		id := r.Path(key)
+		items[i*2] = id
+		items[i*2+1] = val
+	}
+	if err := r.c.MSet(items...).Err(); err != nil {
+		return err
+	}
+	return nil
+}
+
+// Get method
+func (r *Redis) Get(keys ...*Key) ([]string, error) {
+	dirs := make([]string, len(keys))
+	for i, key := range keys {
+		dirs[i] = r.Path(key)
+	}
+	vs, err := r.c.MGet(dirs...).Result()
+	res := make([]string, len(vs))
+	for i, s := range vs {
+		res[i] = s.(string)
+	}
+	if err != nil {
+		return nil, err
+	}
+	return res, nil
+}
+
+// List method
+func (r *Redis) List(key *Key) ([]*Key, error) {
+	temp := &Key{id: key.id, dirs: make([]string, len(key.dirs))}
+	for i, v := range key.dirs {
+		temp.dirs[i] = v
+	}
+	if len(temp.dirs) > 0 {
+		if s := temp.dirs[len(temp.dirs)-1]; !strings.Contains(s, "*") {
+			temp.dirs = append(temp.dirs, "*")
+		}
+	} else {
+		temp.dirs = append(temp.dirs, "*")
+	}
+	tar := strings.TrimSuffix(r.Path(temp), ":*")
+	ids, err := r.c.Keys(r.Path(temp)).Result()
+	if err != nil {
+		return nil, err
+	}
+	keys := make([]*Key, len(ids))
+	for i, id := range ids {
+		pfx := strings.Replace(id, tar, "", -1)
+		str := strings.TrimPrefix(pfx, ":")
+		parts := strings.SplitAfter(str, ":")
+		dirs := make([]string, len(key.dirs)+len(parts))
+		for i, v := range key.dirs {
+			dirs[i] = v
+		}
+		for i, v := range parts {
+			dirs[i+len(key.dirs)] = v
+		}
+		keys[i] = &Key{
+			id:   key.id,
+			dirs: dirs,
+		}
+	}
+	return keys, nil
+}
+
+// Delete method
+func (r *Redis) Delete(keys ...*Key) error {
+	ids := make([]string, len(keys))
+	for i, v := range keys {
+		ids[i] = r.Path(v)
+	}
+	if _, err := r.c.Del(ids...).Result(); err != nil {
+		return err
+	}
+	return nil
+}
